c67eb3e7e5eb66e483bef4b20caa9c53daf43833
Lanai Conceição <conceicao.lanai@gmail.com>
2022-01-25 09:22:14 -0300

    File Structure for React Redux

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './Home';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, world!</h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+  return <h1>Home bar</h1>
+}
+
+export default Home;
